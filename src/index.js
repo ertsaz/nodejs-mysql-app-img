0d0f2a39178e94cbeb20e5147dc9f1583272e9d0
Ernesto <ertsaz9009@gmail.com>
2020-11-10 22:59:06 -0500

    configuracion de vistas, midlewares y las rutas del servidor

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const morgarn = require('morgan');
+const exhbs = require('express-handlebars');
+const path = require('path');
 const app = express();
 require('dotenv').config();
 const pool = require('./database');
 
 //configuracion
 app.set('port', process.env.PORT || 5000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exhbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'parcials'),
+    extname: '.hbs'
+
+}));
+app.set('view engine', '.hbs');
 
 //midleware
 app.use(morgarn('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//routers
+app.use(require('./routers/index'));
+
+//global variables
+
 
 //iniciar server
 app.listen(app.get('port'), () => {
